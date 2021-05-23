b6ba95bbd033945d17a6617ba903b52f08f398ed
Stevie <xxlennygamerxx@gmail.com>
2021-05-23 14:42:15 -0700

    bad apple lol

---
+++ Content/Items/Consumable/Food/BadApple.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ID;
+
+namespace SteviesModRedux.Content.Items.Consumable.Food
+{
+    public class BadApple : FoodItem
+    {
+        public override int FoodBuff => BuffID.Poisoned;
+
+        public override int BuffDuration => 60 * 1000;
+
+        public override Color[] FoodParticleColors => new[]
+            {Color.Green, Color.GreenYellow, Color.DarkOliveGreen};
+
+        public override void SafeSetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bad Apple (lol)");
+            Tooltip.SetDefault(":(");
+        }
+    }
+}
