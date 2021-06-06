5b0de065d5907701cf68793cb1f64c23cf25f487
Stevie <xxlennygamerxx@gmail.com>
2021-06-06 00:41:28 -0700

    accurate DPS measurements

---
+++ Content/Globals/Players/DpsTrackerPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SteviesModRedux.Content.Globals.Players
+{
+    public class DpsTrackerPlayer : ModPlayer
+    {
+        public List<(int, uint)> Damages { get; protected set; }
+
+        public override void Initialize()
+        {
+            Damages = new List<(int, uint)>();
+        }
+
+        public override void Load()
+        {
+            On.Terraria.Player.getDPS += DpsHijacker;
+        }
+
+        public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
+        {
+            Damages.Add((damage, Main.GameUpdateCount));
+        }
+
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
+        {
+            Damages.Add((damage, Main.GameUpdateCount));
+        }
+
+        public override void OnHitPvp(Item item, Player target, int damage, bool crit)
+        {
+            Damages.Add((damage, Main.GameUpdateCount));
+        }
+
+        public override void OnHitPvpWithProj(Projectile proj, Player target, int damage, bool crit)
+        {
+            Damages.Add((damage, Main.GameUpdateCount));
+        }
+
+        private static int DpsHijacker(On.Terraria.Player.orig_getDPS orig, Player self)
+        {
+            for (int i = 0; i < self.GetModPlayer<DpsTrackerPlayer>().Damages.Count; i++)
+            {
+                if (self.GetModPlayer<DpsTrackerPlayer>().Damages[i].Item2 < Main.GameUpdateCount - 60) 
+                    self.GetModPlayer<DpsTrackerPlayer>().Damages.RemoveAt(i);
+            }
+
+            return self.GetModPlayer<DpsTrackerPlayer>().Damages.Sum(d => d.Item1);
+        }
+    }
+}
