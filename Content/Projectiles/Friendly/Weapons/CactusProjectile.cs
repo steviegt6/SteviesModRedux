3ec0888d635b7107cffe43caaf7860ec8a76162c
Stevie <xxlennygamerxx@gmail.com>
2021-07-05 19:17:03 -0700

    terra assets

---
+++ Content/Projectiles/Friendly/Weapons/CactusProjectile.cs
@@ -0,0 +1,10 @@
+﻿namespace SteviesModRedux.Content.Projectiles.Friendly.Weapons
+{
+    public class CactusProjectile : ReduxProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+    }
+}
